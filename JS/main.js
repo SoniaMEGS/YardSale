ac5405c7ff65d0e5cff4e80da19fbf2ca2feed92
SoniaMEGS <megs.sonia@gmail.com>
2023-04-25 14:17:44 -0700

    the navigation bar was merged with the collapsible mobile menu in index.html, was created the function that shows and collapses the mobile menu in main.js and the CSS was modified to let the merger works correctly.

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,16 @@
+const menuBars = document.querySelector('.menu');
+const closeContainers =  document.querySelector('.closeContainer');
+const mobileMenu = document.querySelector('.mobileMenu');
 const menuEmail = document.querySelector('.navEmail');
 const desktopMenu = document.querySelector('.desktopMenu');
 
+menuBars.addEventListener('click', toggleMobileMenu);
+closeContainers.addEventListener('click', toggleMobileMenu);
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
+function toggleMobileMenu (){
+    mobileMenu.classList.toggle('inactive');
+}
 function toggleDesktopMenu (){
     desktopMenu.classList.toggle('inactive');
 }
